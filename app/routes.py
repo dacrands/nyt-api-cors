2033e4cecdfd334e6497eb95158bca04b1b776f7
da crands <davecrands@gmail.com>
2018-09-14 22:00:31 -0400

    Added GET /best

--- app/routes.py
+++ app/routes.py
@@ -8,14 +8,22 @@ def index():
   res = requests.get('https://api.nytimes.com/svc/archive/v1/1915/10.json?api-key=1e2e8c3de5ee427c835460f4a58a4792')
   return res.text
 
+@app.route('/api/best')
+def best():
+  res = requests.get('https://api.nytimes.com/svc/books/v3/lists/current/hardcover-nonfiction.json?api-key={0}'.format(app.config['API_KEY']))
+  if res.status_code != 200:
+    errData = {'status': res.status_code, 'error': 'There was an error'}
+    return jsonify(errData), res.status_code    
+  
+  bestData = jsonify(res.json())
+  return bestData
+
 @app.route('/api/archives/<month>/<year>')
 def archives(month, year):
   res = requests.get('https://api.nytimes.com/svc/archive/v1/{0}/{1}.json?api-key={2}'.format(month, year, app.config['API_KEY']))
-  print(res.status_code)
   if res.status_code != 200:
     errData = {'status': res.status_code, 'error': 'There was an error'}
-    return jsonify(errData)
+    return jsonify(errData), res.status_code    
 
   archivesData =  jsonify(res.json())  
-  
   return archivesData
